bacb61589ad379dc38b607293321cc7ffe183ccd
Marina Gutkevich <gutmalina@mail.ru>
2022-08-23 14:45:20 +0300

    change config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,10 @@
-const {
-  BASE_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : '//localhost:3001',
-  MOVIES_URL = process.env.NODE_ENV === 'production' ? process.env.MOVIES_URL : '//api.nomoreparties.co',
-} = process.env;
+// const {
+//   BASE_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : '//localhost:3001',
+//   MOVIES_URL = process.env.NODE_ENV === 'production' ? process.env.MOVIES_URL : '//api.nomoreparties.co',
+// } = process.env;
+
+const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`;
+const MOVIES_URL = `${window.location.protocol}${process.env.MOVIES_URL || '//api.nomoreparties.co'}`;
 
 module.exports = {
   BASE_URL,
