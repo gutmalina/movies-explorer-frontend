aba5cc563e8770cfde37d8ce16de04a5d77b8097
Marina Gutkevich <gutmalina@mail.ru>
2022-08-24 15:29:14 +0300

    make readme

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`
-const MOVIES_URL = 'https://api.nomoreparties.co'
+const MOVIES_URL = 'http://api.nomoreparties.co'
 
 module.exports = {
   BASE_URL,
