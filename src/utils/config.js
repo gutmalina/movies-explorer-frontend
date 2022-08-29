75f1890b57745adf27dc2b4af3f2fe9ac73a6694
Marina Gutkevich <gutmalina@mail.ru>
2022-08-29 12:42:50 +0300

    collect

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`
+const BASE_URL = 'https://api.webdiploma.nomoredomains.xyz'
 const MOVIES_URL = 'https://api.nomoreparties.co'
 
 module.exports = {
